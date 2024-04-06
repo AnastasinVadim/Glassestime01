91356d5b64570619d3c93a91292f788fb32be25b
AnastasinVadim <143996195+AnastasinVadim@users.noreply.github.com>
2024-04-06 23:52:57 +0300

    Update slider.js

--- slider.js
+++ slider.js
@@ -1,36 +1,34 @@
-jQuery(function ($) {
-    $(document).ready(function() {
-        $(".slider").slick({
-          arrows: true,
-          dots: true,
-          prevArrow: '<button type="button" class="slick-prev">Previous</button>',
-          nextArrow: '<button type="button" class="slick-next">Next</button>',
-          responsive: [
-            {
-              breakpoint: 560,
-              settings: {
-                arrows: true,
-                dots: true,
-                slidesToScroll: 1,
-                slidesToShow: 1
-              }
-            }
-          ],
-          slidesToScroll: 1,
-          slidesToShow: 3
-        });
-        
-        var slider = $(".slider");
-        var currentPage = $("#current-page");
-        var totalPages = $("#total-pages");
+jQuery(document).ready(function($) {
+    $(".slider").slick({
+      arrows: true,
+      dots: true,
+      prevArrow: '<button type="button" class="slick-prev">Previous</button>',
+      nextArrow: '<button type="button" class="slick-next">Next</button>',
+      responsive: [
+        {
+          breakpoint: 560,
+          settings: {
+            arrows: true,
+            dots: true,
+            slidesToScroll: 1,
+            slidesToShow: 1
+          }
+        }
+      ],
+      slidesToScroll: 1,
+      slidesToShow: 3
+    });
     
-        slider.on("afterChange", function(event, slick, currentSlide) {
-          currentPage.text(currentSlide + 1);
-        });
-    
-        slider.on("init reInit afterChange", function(event, slick, currentSlide) {
-          var slides = slick.slideCount;
-          totalPages.text(slides);
-        });
-      });
+    var slider = $(".slider");
+    var currentPage = $("#current-page");
+    var totalPages = $("#total-pages");
+
+    slider.on("afterChange", function(event, slick, currentSlide) {
+      currentPage.text(currentSlide + 1);
+    });
+
+    slider.on("init reInit afterChange", function(event, slick, currentSlide) {
+      var slides = slick.slideCount;
+      totalPages.text(slides);
+    });
 });
